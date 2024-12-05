e6fc15cb7350197f2681987a57872e5daa327b10
ymurillo89 <murilloruiz91@gmail.com>
2024-12-05 16:37:41 -0500

    Preparatoría para los otros ejercicios. de widgets

---
+++ lib/presentation/screen/progress/app_tutorial_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ApTutorialScreen extends StatelessWidget {
+ 
+  static const name = "AnimatedScreen";
+ 
+  const ApTutorialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
